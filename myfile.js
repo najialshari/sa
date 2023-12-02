a023a0ac9277dd389c830b476900865390a5e721
Naji AlShari <najigoon2@gmail.com>
2023-12-02 14:20:35 +0300

    Initial app

---
+++ myfile.js
@@ -0,0 +1,14 @@
+var myvar = 5
+if (true)
+{
+  const myconst = 100
+    console.log("My const is", myconst)
+  
+  let  mylet = 1000
+  mylet = 1100
+  console.log("My let is", mylet)
+}
+myvar = 55
+console.log("My var is", myvar)
+var textArr= {name:"ali"}
+console.log("My text[0] is", textArr[0])
